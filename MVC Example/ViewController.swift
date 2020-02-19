026f2a4d08ae5a92ba6b5270b15b50cd1cbcea93
Yuriy <178mamonov@mail.ru>
2020-02-19 22:29:42 +0300

    Initial Commit

---
+++ MVC Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC Example
+//
+//  Created by Юрий Мамонов on 19.02.2020.
+//  Copyright © 2020 Yuriy Mamonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
